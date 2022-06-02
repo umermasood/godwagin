0d404e01f45a6d60476b7ebb297ac72264fef1af
umermasood <umermasood.dev@gmail.com>
2022-06-02 17:29:55 +0500

    PUT /recipes/id Endpoint - Update an existing recipe

--- main.go
+++ main.go
@@ -40,6 +40,30 @@ func ListRecipesHandler(context *gin.Context) {
 	context.JSON(http.StatusOK, recipes)
 }
 
+func UpdateRecipeHandler(context *gin.Context) {
+	id := context.Param("id")
+
+	var recipe Recipe
+	if err := context.ShouldBindJSON(&recipe); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	for i, recp := range recipes {
+		if recp.ID == id {
+			recipes[i] = recipe
+			context.JSON(http.StatusOK, recipe)
+			return
+		}
+	}
+
+	context.JSON(http.StatusNotFound, gin.H{
+		"error": "Recipe not found!",
+	})
+}
+
 func init() {
 	recipes = make([]Recipe, 0)
 	file, _ := ioutil.ReadFile("recipes.json")
@@ -52,6 +76,7 @@ func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	if err := router.Run(); err != nil {
 		return
 	}
