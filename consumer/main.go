2524fd5c918535a0f04f7728ec8367e2197bfca0
M Umer Masood <umermasood.dev@gmail.com>
2023-02-28 19:22:26 +0500

    RabbitMQ consumer service setup

---
+++ consumer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+	"os"
+)
+
+func main() {
+	amqpConnection, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer amqpConnection.Close()
+
+	channelAmqp, _ := amqpConnection.Channel()
+	defer channelAmqp.Close()
+
+	forever := make(chan bool)
+
+	msgs, err := channelAmqp.Consume(
+		os.Getenv("RABBITMQ_QUEUE"),
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
