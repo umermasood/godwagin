4a937cd50b02b6d6416b6484ea028d77089b318e
M Umer Masood <umermasood.dev@gmail.com>
2023-03-01 14:31:17 +0500

    consumer service saves feed entries in mongodb

--- consumer/main.go
+++ consumer/main.go
@@ -1,12 +1,73 @@
 package main
 
 import (
-	"github.com/streadway/amqp"
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Request struct {
+	URL string `json:"url"`
+}
+
+type Feed struct {
+	Entries []Entry `xml:"entry"`
+}
+
+type Entry struct {
+	Link struct {
+		Href string `xml:"href,attr"`
+	} `xml:"link"`
+	Thumbnail struct {
+		URL string `xml:"url,attr"`
+	} `xml:"thumbnail"`
+	Title string `xml:"title"`
+}
+
+func GetFeedEntries(url string) ([]Entry, error) {
+	httpClient := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(resp.Body)
+
+	byteValue, _ := ioutil.ReadAll(resp.Body)
+	var feed Feed
+	err = xml.Unmarshal(byteValue, &feed)
+	if err != nil {
+		return nil, err
+	}
+
+	return feed.Entries, nil
+}
 func main() {
+	ctx := context.Background()
+	mongoClient, _ := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	defer mongoClient.Disconnect(ctx)
+
 	amqpConnection, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
 	if err != nil {
 		log.Fatal(err)
@@ -31,6 +92,27 @@ func main() {
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
+
+			var request Request
+			json.Unmarshal(d.Body, &request)
+
+			log.Println("RSS URL:", request.URL)
+
+			entries, _ := GetFeedEntries(request.URL)
+
+			fmt.Println(entries)
+
+			collection := mongoClient.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+			for _, entry := range entries[2:] {
+				_, err := collection.InsertOne(ctx, bson.M{
+					"title":     entry.Title,
+					"thumbnail": entry.Thumbnail.URL,
+					"url":       entry.Link.Href,
+				})
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
 		}
 	}()
 
